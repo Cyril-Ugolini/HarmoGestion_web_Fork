c1c43ffc08ad2ff1d3655d92745aa1c375c20134
Cédric DIDIER <cedric.did@gmail.com>
2026-04-08 08:24:56 +0200

    docs : modification du MCD et mise à jour du script de création

--- DossierConception/SQL/ScriptCreationBDD.sql
+++ DossierConception/SQL/ScriptCreationBDD.sql
@@ -3,68 +3,68 @@ CREATE DATABASE harmogestion;
 USE harmogestion;
 
 CREATE TABLE Membre(
-   idMembre INT AUTO_INCREMENT,
-   nomMembre VARCHAR(30) NOT NULL,
-   prenomMembre VARCHAR(30) NOT NULL,
-   dateInscriptionMembre DATE NOT NULL,
-   PRIMARY KEY(idMembre)
+   id_membre INT AUTO_INCREMENT,
+   nom_membre VARCHAR(30) NOT NULL,
+   prenom_membre VARCHAR(30) NOT NULL,
+   date_inscription_membre DATE NOT NULL,
+   PRIMARY KEY(id_membre)
 )ENGINE=InnoDB;
 
 CREATE TABLE Instrument(
-   idInstrument INT AUTO_INCREMENT,
-   libelleInstrument VARCHAR(50) NOT NULL,
-   PRIMARY KEY(idInstrument),
-   UNIQUE(libelleInstrument)
+   id_instrument INT AUTO_INCREMENT,
+   libelle_instrument VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id_instrument),
+   UNIQUE(libelle_instrument)
 )ENGINE=InnoDB;
 
 CREATE TABLE Cours(
-   idCours INT AUTO_INCREMENT,
-   dateCours DATETIME NOT NULL,
-   dureeCours TINYINT NOT NULL,
-   idInstrument INT NOT NULL,
-   idMembreEnseignant INT NOT NULL,
-   PRIMARY KEY(idCours),
-   FOREIGN KEY(idInstrument) REFERENCES Instrument(idInstrument),
-   FOREIGN KEY(idMembreEnseignant) REFERENCES Membre(idMembre)
+   id_cours INT AUTO_INCREMENT,
+   date_cours DATETIME NOT NULL,
+   duree_cours TINYINT NOT NULL,
+   id_instrument INT NOT NULL,
+   id_membre_enseignant INT NOT NULL,
+   PRIMARY KEY(id_cours),
+   FOREIGN KEY(id_instrument) REFERENCES Instrument(id_instrument),
+   FOREIGN KEY(id_membre_enseignant) REFERENCES Membre(id_membre)
 )ENGINE=InnoDB;
 
 CREATE TABLE Representation(
-   idRepresentation INT AUTO_INCREMENT,
-   nomRepresentation VARCHAR(80) NOT NULL,
-   dateRepresentation DATETIME NOT NULL,
-   lieuRepresentation VARCHAR(100) NOT NULL,
-   PRIMARY KEY(idRepresentation)
+   id_representation INT AUTO_INCREMENT,
+   nom_representation VARCHAR(80) NOT NULL,
+   date_representation DATETIME NOT NULL,
+   lieu_representation VARCHAR(100) NOT NULL,
+   PRIMARY KEY(id_representation)
 )ENGINE=InnoDB;
 
 CREATE TABLE Pratiquer(
-   idMembre INT,
-   idInstrument INT,
-   enCoursApprentissage BOOLEAN NOT NULL,
-   PRIMARY KEY(idMembre, idInstrument),
-   FOREIGN KEY(idMembre) REFERENCES Membre(idMembre),
-   FOREIGN KEY(idInstrument) REFERENCES Instrument(idInstrument)
+   id_membre INT,
+   id_instrument INT,
+   apprentissage_en_cours BOOLEAN NOT NULL,
+   PRIMARY KEY(id_membre, id_instrument),
+   FOREIGN KEY(id_membre) REFERENCES Membre(id_membre),
+   FOREIGN KEY(id_instrument) REFERENCES Instrument(id_instrument)
 )ENGINE=InnoDB;
 
 CREATE TABLE ParticiperCours(
-   idMembreApprenant INT,
-   idCours INT,
-   PRIMARY KEY(idMembreApprenant, idCours),
-   FOREIGN KEY(idMembreApprenant) REFERENCES Membre(idMembre),
-   FOREIGN KEY(idCours) REFERENCES Cours(idCours)
+   id_membre_apprenant INT,
+   id_cours INT,
+   PRIMARY KEY(id_membre_apprenant, id_cours),
+   FOREIGN KEY(id_membre_apprenant) REFERENCES Membre(id_membre),
+   FOREIGN KEY(id_cours) REFERENCES Cours(id_cours)
 )ENGINE=InnoDB;
 
 CREATE TABLE ParticiperRepresentation(
-   idMembre INT,
-   idRepresentation INT,
-   PRIMARY KEY(idMembre, idRepresentation),
-   FOREIGN KEY(idMembre) REFERENCES Membre(idMembre),
-   FOREIGN KEY(idRepresentation) REFERENCES Representation(idRepresentation)
+   id_membre INT,
+   id_representation INT,
+   PRIMARY KEY(id_membre, id_representation),
+   FOREIGN KEY(id_membre) REFERENCES Membre(id_membre),
+   FOREIGN KEY(id_representation) REFERENCES Representation(id_representation)
 )ENGINE=InnoDB;
 
 CREATE TABLE InstrumentsRepresentation(
-   idInstrument INT,
-   idRepresentation INT,
-   PRIMARY KEY(idInstrument, idRepresentation),
-   FOREIGN KEY(idInstrument) REFERENCES Instrument(idInstrument),
-   FOREIGN KEY(idRepresentation) REFERENCES Representation(idRepresentation)
+   id_instrument INT,
+   id_representation INT,
+   PRIMARY KEY(id_instrument, id_representation),
+   FOREIGN KEY(id_instrument) REFERENCES Instrument(id_instrument),
+   FOREIGN KEY(id_representation) REFERENCES Representation(id_representation)
 )ENGINE=InnoDB;
