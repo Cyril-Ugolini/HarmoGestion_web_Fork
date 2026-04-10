4a85e8962f5c1c03a82a178c65fbdaa348a16d10
Cédric DIDIER <cedric.did@gmail.com>
2026-04-10 12:00:20 +0200

    refactor: W.I.P. ajout du contrôleur des pages liées aux instruments

--- src/main/java/fr/afpa/cda19/harmogestionweb/controllers/ControllerPagesInstruments.java
+++ src/main/java/fr/afpa/cda19/harmogestionweb/controllers/ControllerPagesInstruments.java
@@ -1,22 +1,45 @@
 package fr.afpa.cda19.harmogestionweb.controllers;
 
+import fr.afpa.cda19.harmogestionweb.models.Instrument;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Classe de controller liée aux membres.
+ * Classe de controller liée aux instruments.
+ *
+ * @author Cédric DIDIER
+ * @version 1.0.0
+ * @since 10/04/2026
  */
 @Controller
 public class ControllerPagesInstruments {
-    //==== Méthodes ====
+
     /**
-     * Méthode d'accès à la page d'ajout d'instrument'.
+     * Méthode d'accès à la page d'ajout d'instrument.
+     *
      * @param model Modèle de la page.
      * @return URI de la page.
      */
     @GetMapping("/ajouterInstrument")
     public String ajouterInstrument(Model model) {
-        return "ajouterInstrument";
+        model.addAttribute("action", "/ajouterInstrument");
+        model.addAttribute("nomAction", "Enregistrer");
+        model.addAttribute("titreFormulaire", "Ajouter un instrument");
+        model.addAttribute("titrePage", "HarmoGestion : Ajout d'un instrument");
+        return "formulaireInstrument";
+    }
+
+    @PostMapping("/ajouterInstrument")
+    public ModelAndView creerInstrument(
+            @ModelAttribute
+            @Valid
+            final Instrument instrument, final BindingResult result) {
+        return null;
     }
 }
